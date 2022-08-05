b403526310dca9589cf267764066911efe1bcc26
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-05 16:15:06 -0300

    view: Adiciona estilo do componente de linhas de detalhe

---
+++ src/global/styles-components/Linhas/linhas.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const ContainerLinhas = styled.View`
+  display: flex;
+  width: 100%;
+  margin: 15% 0;
+`;
+
+export const LinhaCinza = styled.View`
+  border: 1px solid #e2e2e2;
+`;
+
+export const LinhaLaranja = styled.View`
+  border: 1px solid #fb9400;
+  margin: -2px 15%;
+`;
