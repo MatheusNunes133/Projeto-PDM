c0bc4cf8f972114cbf3aadb1ec34965d4af3c758
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-10 22:37:49 -0300

    view: Adicona estilo para tela de Mensagem

---
+++ src/global/styles/Mensagem/mensagem.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Header = styled.View`
+  display: flex;
+  align-items: center;
+  margin: 15% 5% 20% 5%;
+`;
+
+export const ImageMessage = styled.Image``;
