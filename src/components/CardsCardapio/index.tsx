3b1c5fe7350c950733c2f3f56a722d71dc4cabbb
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-14 21:21:31 -0300

    feat: Adiciona componente CardsCardapio

---
+++ src/components/CardsCardapio/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  ContainerCards,
+  CardsImage,
+  CardsInfo,
+  HamburguerType,
+  HamburguerName,
+  HamburguerPrice,
+} from "../../global/styles-components/CardsCardapio/cardsCardapio";
+
+export default function CardsCardapio({
+  image,
+  hamburguerType,
+  hamburguerName,
+  hamburguerPrice,
+  funcao,
+}: PropsCardsCardapio) {
+  return (
+    <ContainerCards onPress={funcao}>
+      <CardsImage source={image} />
+      <CardsInfo>
+        <HamburguerType>{hamburguerType}</HamburguerType>
+        <HamburguerName>{hamburguerName}</HamburguerName>
+        <HamburguerPrice>{hamburguerPrice}</HamburguerPrice>
+      </CardsInfo>
+    </ContainerCards>
+  );
+}
