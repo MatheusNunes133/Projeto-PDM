a1cc0ff4ceffcf7e9038ef177a66c140f17106e9
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-17 16:58:40 -0300

    feat: Adiciona componente para customizar o drawer navigation

---
+++ src/components/CustomDrawer/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+
+import Linhas from "../Linhas";
+
+import {
+  Container,
+  ButtonLogin,
+  TextButton,
+} from "../../global/styles-components/CustomDrawer/customDrawer";
+
+export default function CustomDrawer(props: DrawerContentComponentProps) {
+  return (
+    <Container>
+      <ButtonLogin
+        onPress={() => {
+          const { navigation } = props;
+          console.log(navigation.navigate("Login"));
+        }}
+      >
+        <TextButton>Fazer Login</TextButton>
+      </ButtonLogin>
+      <Linhas />
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </Container>
+  );
+}
