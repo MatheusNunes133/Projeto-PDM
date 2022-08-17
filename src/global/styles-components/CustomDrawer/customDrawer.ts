1a79d355e74ea0a79260af24a422d57e1151b23d
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-17 16:59:36 -0300

    view: Adiciona estilo do componente que customiza o drawer navigation

---
+++ src/global/styles-components/CustomDrawer/customDrawer.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  displat: flex;
+  flex: 1;
+`;
+
+export const ButtonLogin = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10% 5% 5% 5%;
+  background-color: #fb9400;
+  border-radius: 4px;
+  height: 56px;
+`;
+
+export const TextButton = styled.Text`
+  color: #fff;
+`;
