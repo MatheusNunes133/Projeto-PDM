6d2f830999fb88a902207bd8f500c5a62de0d00c
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-09-08 13:27:39 -0300

    fix: Ajusta estados

--- src/components/Sacola/Burger.js
+++ src/components/Sacola/Burger.js
@@ -1,115 +1,122 @@
 import React, { useState } from "react";
-import { View, Image , Text , StyleSheet, TouchableOpacity} from "react-native";
-import burger from '../../../assets/images/img-Texas-Burger.png'
+import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
+import burger from "../../../assets/images/img-Texas-Burger.png";
 
+export default function Burger({ item: { image, subname, name, preco } }) {
+  const [numPedidos, setNumPedidos] = useState(0);
 
-export default function Burger( {item: { image, subname, name, preco}}){
+  return (
+    <View style={styles.container}>
+      <Image source={burger} style={styles.imagem} />
 
+      <View>
+        <Text style={styles.subname}>{subname}</Text>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.preco}>{preco}</Text>
+      </View>
 
+      <View style={styles.quantItens}>
+        <TouchableOpacity
+          style={styles.quantItens1}
+          onPress={() => {
+            if (numPedidos < 1) {
+              setNumPedidos(0);
+            } else {
+              setNumPedidos(numPedidos - 1);
+            }
+          }}
+        >
+          <Text style={styles.subtracao}>-</Text>
+        </TouchableOpacity>
 
-    return <View style={styles.container}>
-        <Image source={ burger } style={styles.imagem} />
-
-        <View>
-            <Text style={styles.subname} >{subname}</Text>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.preco}>{preco}</Text>
+        <View style={styles.quantItens2}>
+          <Text style={styles.number}>{numPedidos}</Text>
         </View>
 
-        <View style={styles.quantItens}>
-
-            <TouchableOpacity style={styles.quantItens1}>
-                <Text style={styles.subtracao}>-</Text>
-            </TouchableOpacity>
-
-
-            <View style={styles.quantItens2}>
-                <Text style={styles.number}>01</Text>
-            </View>
-
-
-            <TouchableOpacity style={styles.quantItens3}>
-                <Text style={styles.adicao}>+</Text>
-            </TouchableOpacity>
-
-        </View>
+        <TouchableOpacity
+          style={styles.quantItens3}
+          onPress={() => {
+            setNumPedidos(numPedidos + 1);
+          }}
+        >
+          <Text style={styles.adicao}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-
+  );
 }
 
 const styles = StyleSheet.create({
-    container : {
-        width: "100%",
-        paddingVertical: 12,
-        alignSelf: 'center',
-        borderBottomColor: '#e5e5e5',
-        borderBottomWidth: 2,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    imagem : {
-        width: 85,
-        height: 85,
-        marginRight: 18,
-    },
-    subname : {
-        color: '#ccc',
-        fontSize: 12,
-    },
-    name: {
-        fontSize: 16,
-        fontWeight: '500',
-    },
-    preco: {
-        fontSize: 16,
-        color: '#fb9400',
-        fontWeight: 'bold',
-    },
-    quantItens : {
-        width: 123,
-        height: 42,
-        display: 'flex',
-        flexDirection: 'row',
-        position: 'absolute',
-        right: 0,
-    },
-    quantItens1 : {
-        backgroundColor: '#f2f4f5',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderBottomLeftRadius: 4,
-        borderTopLeftRadius: 4,
-    },
-    quantItens2 : {
-        width: 40,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    quantItens3 : {
-        backgroundColor: '#fb9400',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        borderBottomRightRadius: 4,
-        borderTopRightRadius: 4,
-    },
-    adicao: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold', 
-    },
-    subtracao: {
-        color: '#96a3ab',
-        fontSize: 16,
-        fontWeight: 'bold', 
-    },
-    number: {
-        color: '#fb9400',
-        fontSize: 16,
-        fontWeight: 'bold', 
-    }
-    
-
-})
+  container: {
+    width: "100%",
+    paddingVertical: 12,
+    alignSelf: "center",
+    borderBottomColor: "#e5e5e5",
+    borderBottomWidth: 2,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  imagem: {
+    width: 85,
+    height: 85,
+    marginRight: 18,
+  },
+  subname: {
+    color: "#ccc",
+    fontSize: 12,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  preco: {
+    fontSize: 16,
+    color: "#fb9400",
+    fontWeight: "bold",
+  },
+  quantItens: {
+    width: 123,
+    height: 42,
+    display: "flex",
+    flexDirection: "row",
+    position: "absolute",
+    right: 0,
+  },
+  quantItens1: {
+    backgroundColor: "#f2f4f5",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomLeftRadius: 4,
+    borderTopLeftRadius: 4,
+  },
+  quantItens2: {
+    width: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  quantItens3: {
+    backgroundColor: "#fb9400",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    borderBottomRightRadius: 4,
+    borderTopRightRadius: 4,
+  },
+  adicao: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  subtracao: {
+    color: "#96a3ab",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  number: {
+    color: "#fb9400",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
