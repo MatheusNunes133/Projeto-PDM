d6e8d5789998e7e49ab69ca063866b74bb019801
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2022-09-10 16:36:12 -0300

    feat: adiciona upload de imagem no perfil

--- src/global/styles-components/Inputs/inputs.ts
+++ src/global/styles-components/Inputs/inputs.ts
@@ -7,3 +7,11 @@ export const Input = styled.TextInput`
   margin-bottom: 30px;
   border-radius: 5px;
 `;
+export const InputButton = styled.Button`
+  height: 60px;
+  background-color: #f9f9fb;
+  padding: 0 5%;
+  margin-bottom: 30px;
+  border-radius: 5px;
+`;
+
