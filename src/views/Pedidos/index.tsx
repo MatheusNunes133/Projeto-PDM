807d3a53435356623836a09fa83d2c4a0295e9d1
Wotson Sula <wotson.sula@smartnx.io>
2022-09-10 19:58:57 -0300

    configurando requisições da tela de Pedidos

--- src/views/Pedidos/index.tsx
+++ src/views/Pedidos/index.tsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View, BackHandler } from "react-native";
+import api from "../../../Api";
+
 import Topo from "../../components/Topo/Topo";
-import { FlatList, StyleSheet, View } from "react-native";
 import Pedido from "./components/Pedido";
-import itens from "../../components/mock/pedidos";
 
 export default function Pedidos({ navigation }: PropsNavigation) {
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      return true;
+    });
+  }, []);
+
+  const [listItems, setItem] = useState([]);
+
+  useEffect(() => {
+    async function teste() {
+      try {
+        let resp = await api.get('/pedidos', {
+          headers: {
+            'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYWlsQG1haWwuY29tIiwiZXhwIjoxNjYyODUyMjc4fQ.5JRzae4n3TZgSVe5X3CX9HMCP_UuAx4LJmsHsSgoLeDSaSqla4mRq7hLHft9e_9J513HQN_TZkUZPwiCHvc2fQ`
+          }
+        })
+        
+        //api.get("/pedidos");
+        setItem(resp.data);
+        //setItem(resp.data);
+      } catch (erro) {
+        console.log(erro);
+      }
+    }
+    teste();
+  }, []);
+  console.log(listItems);
+
   return (
     <>
       <FlatList
-        data={itens}
+        data={listItems}
         renderItem={Pedido}
-        keyExtractor={() => {}}
+        keyExtractor={(item) => item.id}
         ListHeaderComponent={() => {
           return (
             <View style={styles.bottom}>
